d1dab119b8708e9b6abdc7bac6e2098a51395049
Colin <colingilbert86@gmail.com>
2020-11-11 22:55:08 +0000

    Make a chain!

--- UserApp.cpp
+++ UserApp.cpp
@@ -4,11 +4,10 @@
 std::unique_ptr<std::string> message_profiling, message_collision;
 
 noob::actor_handle ah;
-
+noob::prop_handle ph;
 noob::font example_font;
 
 std::string test_message;
-
 
 noob::vec3f random_vec3(float x, float y, float z)
 {
@@ -30,78 +29,82 @@ bool noob::application::user_init()
 
 	noob::globals& g = noob::get_globals();
 
-	const noob::shape_handle scenery_shp = g.sphere_shape(50.0); // g.box_shape(50.0, 20.0, 50.0);
-	const noob::scenery_handle sc_h = stage.scenery(scenery_shp, noob::vec3f(0.0, 0.0, 0.0), noob::versorf(0.0, 0.0, 0.0, 1.0));
+	const noob::shape_handle scenery_shp = g.box_shape(100.0, 1.0, 100.0);//sphere_shape(50.0); 
+	const noob::scenery_handle sc_h = stage.create_scenery(scenery_shp, noob::vec3f(0.0, 0.0, 0.0), noob::versorf(0.0, 0.0, 0.0, 1.0));
 
 	noob::reflectance r;
 	r.set_specular(noob::vec3f(0.1, 0.1, 0.1));
 	r.set_diffuse(noob::vec3f(0.1, 0.1, 0.1));
 	r.set_emissive(noob::vec3f(0.0, 0.0, 0.0));
-	r.set_shine(8.0);
+	r.set_shine(0.8);
 	r.set_albedo(0.3);
 	r.set_fresnel(0.2);
 
 	const noob::reflectance_handle rh = g.reflectances.add(r);
 
-	const float actor_dims = 2.0;
+	const float actor_dims = 1.0;
 	// const noob::shape_handle shp = g.sphere_shape(actor_dims);
-	
-	const noob::shape_handle actor_shp = g.sphere_shape(actor_dims);
-
+	const noob::shape_handle unit_cube_shp = g.box_shape(1.0, 1.0, 1.0); // sphere_shape(actor_dims);
+	const noob::shape_handle unit_sphere_shp = g.sphere_shape(0.5);
 	const uint32_t actor_count = 50;
-
-	// TODO: Fixup
+/*
 	noob::actor_blueprints bp;
-	bp.bounds = actor_shp;
+	bp.bounds = unit_cube;
 	bp.reflect = rh;
-	bp.model = g.model_from_shape(actor_shp, actor_count*8);
+	bp.model = g.model_from_shape(unit_cube, actor_count*8);
 
-	noob::actor_blueprints_handle bph = stage.add_actor_blueprints(bp);
+	noob::actor_blueprints_handle abph = stage.add_actor_blueprints(bp);
 
-	const float stage_dim = static_cast<float>(actor_count);
-
-	stage.reserve_actors(bph, actor_count * 8);
+	stage.reserve_actors(abph, actor_count * 8);
 
 	for (uint32_t i = 0; i < actor_count; ++i)
 	{	
-		ah = stage.actor(bph, 0, random_vec3(stage_dim, stage_dim, stage_dim), random_versor());
-	}
-/*	
-	for (uint32_t i = 0; i < actor_count; ++i)
-	{	
-		ah = stage.actor(bph, 1, random_vec3(stage_dim, stage_dim, -stage_dim), random_versor());
+		ah = stage.create_actor(abph, 0, random_vec3(stage_dim, stage_dim, stage_dim), random_versor());
 	}
 
-	for (uint32_t i = 0; i < actor_count; ++i)
-	{	
-		ah = stage.actor(bph, 2, random_vec3(stage_dim, -stage_dim, stage_dim), random_versor());
-	}
-
-	for (uint32_t i = 0; i < actor_count; ++i)
-	{	
-		ah = stage.actor(bph, 3, random_vec3(stage_dim, -stage_dim, -stage_dim), random_versor());
-	}
-
-	for (uint32_t i = 0; i < actor_count; ++i)
-	{	
-		ah = stage.actor(bph, 4, random_vec3(-stage_dim, stage_dim, stage_dim), random_versor());
-	}
-	
-	for (uint32_t i = 0; i < actor_count; ++i)
-	{	
-		ah = stage.actor(bph, 5, random_vec3(-stage_dim, stage_dim, -stage_dim), random_versor());
-	}
-
-	for (uint32_t i = 0; i < actor_count; ++i)
-	{	
-		ah = stage.actor(bph, 6, random_vec3(-stage_dim, -stage_dim, stage_dim), random_versor());
-	}
-
-	for (uint32_t i = 0; i < actor_count; ++i)
-	{	
-		ah = stage.actor(bph, 7, random_vec3(-stage_dim, -stage_dim, -stage_dim), random_versor());
-	}
+	logger::log(noob::importance::INFO, "Done making actors");
 */
+
+	noob::scenery_handle sc2_h = stage.create_scenery(unit_cube_shp, noob::vec3f(0.0, 50.0, 0.0), noob::versorf(0.0, 0.0, 0.0, 1.0));
+
+	const size_t prop_count = 30;
+	const float stage_dim = static_cast<float>(prop_count);
+	noob::prop_blueprints pb;
+	pb.mass = 5.0;
+	pb.model =  g.model_from_shape(unit_sphere_shp, prop_count*8);
+	pb.shape = unit_cube_shp;
+	pb.reflect = rh;
+	pb.ccd = false;
+	
+	noob::prop_blueprints_handle pbph = stage.add_props_blueprints(pb);
+
+	stage.reserve_props(pbph, prop_count * 8);
+	float y_accum = 38.0;
+	noob::body_handle last_bod = stage.get_scenery(sc2_h).body;
+	for (uint32_t i = 0; i < prop_count; ++i)
+	{
+		ph = stage.create_prop(pbph, 0, noob::vec3f(0.0, y_accum, 0.0), noob::versorf(0.0, 0.0, 0.0, 1.0));
+		noob::body_handle current_bod = stage.get_prop(ph).body;
+		noob::constraint_handle pc = stage.create_point_constraint(current_bod, last_bod, noob::vec3f(0.0, 0.5, 0.0), noob::vec3f(0.0, 0.0, 0.0)); 
+		
+		y_accum -= 1.0;
+		last_bod = current_bod;
+	}
+
+	noob::shape_handle large_sphere_shp = g.sphere_shape(7.0);
+	noob::prop_blueprints wrecking_ball_bp;
+	wrecking_ball_bp.mass = 1000.0;
+	wrecking_ball_bp.shape = large_sphere_shp;
+	wrecking_ball_bp.model = g.model_from_shape(large_sphere_shp, 10);
+	wrecking_ball_bp.reflect = rh;
+	wrecking_ball_bp.ccd= true;
+
+	auto wrecking_ball_bp_h = stage.add_props_blueprints(wrecking_ball_bp);
+	stage.reserve_props(wrecking_ball_bp_h, 10);
+	auto wrecking_ball_h = stage.create_prop(wrecking_ball_bp_h, 0, noob::vec3f(0.0, y_accum + 15.0, 0.0), noob::versorf(0.0, 0.0, 0.0, 1.0));
+	
+	auto wrecking_ball_bod = stage.get_prop(wrecking_ball_h).body;
+	auto constrain_h = stage.create_point_constraint(wrecking_ball_bod, last_bod, noob::vec3f(0.0, 7.0, 0.0), noob::vec3f(0.0, 0.5, 0.0));
 
 	// const noob::vec2d test_val = noob::vec2d(1.0, 0.7);
 	// uint32_t db_idx = db.vec2fp_add(test_val);
@@ -109,6 +112,7 @@ bool noob::application::user_init()
 	// noob::logger::log(noob::importance::INFO, noob::concat("[Application] Inserted value: ", noob::to_string(test_val), ". Returned value: ", noob::to_string(ret_val), "."));
 
 	logger::log(noob::importance::INFO, "[Application] Successfully done (C++) user init.");
+	paused = false;
 
 	return true;
 }
@@ -116,14 +120,13 @@ bool noob::application::user_init()
 
 void noob::application::user_update(double dt)
 {
-	
 	app_gui.text("Happy happy fun fun!!!!", noob::vec2f(100.0, 100.0), noob::gui::font_size::READING, noob::vec4f(1.0, 1.0, 0.0, 1.0));
 	
 	noob::globals& g = noob::get_globals();
-	const noob::time nowtime = noob::clock::now();
+	const noob::time_point nowtime = noob::clock::now();
 	const noob::duration time_since_update = nowtime - last_ui_update;
 	const uint64_t profiling_interval = 3000;
-	
+
 	if (noob::millis(time_since_update) > profiling_interval - 1)
 	{
 		const noob::profiler_snap snap = g.profile_run;
